b7f503c0fd13f25c2171c1a16bc1b8a45d59f2ac
양혜리 <yangyang6424@gmail.com>
2019-09-25 01:09:05 +0900

    [Feature] 백준 2447번 풀이 완료

---
+++ Algorithm/Algorithm/Baekjoon2447/Baekjoon2447.c
@@ -0,0 +1,54 @@
+//
+//  Baekjoon2447.c
+//  Algorithm
+//
+//  Created by 양혜리 on 25/09/2019.
+//  Copyright © 2019 hryang. All rights reserved.
+//
+
+#include "Baekjoon2447.h"
+
+char map[3000][3000];
+
+void init(int n)      // 맵 초기화
+{
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            map[i][j] = ' ';
+        }
+    }
+}
+
+void solve(int n, int x, int y)
+{
+    
+    if (n == 1) {     // n이 1이면 현재 위치 (map[x][y]를 *로 채움
+        map[x][y] = '*';
+        return;
+    }
+    int div = n / 3;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (i == 1 && j == 1) {     // (0,1,2) (0,1,2)로 보았을때 1은 중간 위치이므로
+                continue;                  // i가 1이고 j가 1이면 공백으로 처리
+            }
+            solve(div, x + (div * i), y + (div * j));  // 3으로 나누어서 문제를 해결
+        }
+    }
+    return;
+}
+
+void printBoarding() {
+    int n;
+    scanf("%d", &n);
+    
+    init(n);
+    solve(n, 0, 0);
+    
+    for (int i = 0; i < n; ++i) {     // 맵 출력
+        for (int j = 0; j < n; ++j) {
+            printf("%c", map[i][j]);
+        }
+    printf("\n");
+    }
+}
---
+++ Algorithm/Algorithm/Baekjoon2447/Baekjoon2447.h
@@ -0,0 +1,14 @@
+//
+//  Baekjoon2447.h
+//  Algorithm
+//
+//  Created by 양혜리 on 25/09/2019.
+//  Copyright © 2019 hryang. All rights reserved.
+//
+
+#ifndef Baekjoon2447_h
+#define Baekjoon2447_h
+
+#include <stdio.h>
+void printBoarding(void);
+#endif /* Baekjoon2447_h */
