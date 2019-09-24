80a553f81d82f4b9e459401946e348965978d33f
양혜리 <yangyang6424@gmail.com>
2019-09-24 15:55:28 +0900

    [Feature] 백준 1000번 풀이 완료

---
+++ Algorithm/Algorithm/Baekjoon1000/Baekjoon1000.c
@@ -0,0 +1,16 @@
+//
+//  Baekjoon1000.c
+//  Algorithm
+//
+//  Created by 양혜리 on 24/09/2019.
+//  Copyright © 2019 hryang. All rights reserved.
+//
+
+#include "Baekjoon1000.h"
+
+void solution() {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a+b);
+}
---
+++ Algorithm/Algorithm/Baekjoon1000/Baekjoon1000.h
@@ -0,0 +1,14 @@
+//
+//  Baekjoon1000.h
+//  Algorithm
+//
+//  Created by 양혜리 on 24/09/2019.
+//  Copyright © 2019 hryang. All rights reserved.
+//
+
+#ifndef Baekjoon1000_h
+#define Baekjoon1000_h
+
+#include <stdio.h>
+void solution();
+#endif /* Baekjoon1000_h */
