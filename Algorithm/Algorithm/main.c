62106dfef7eb85743162da561ebb40e1e315b76e
양혜리 <yangyang6424@gmail.com>
2019-09-24 15:31:20 +0900

    [Create] C 프로젝트 생성

---
+++ Algorithm/Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithm
+//
+//  Created by 양혜리 on 24/09/2019.
+//  Copyright © 2019 hryang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
